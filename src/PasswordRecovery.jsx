b3d009ce7411fa643495127a040c4a9a7cb9aba9
nrm00014 <nrm00014@mix.wvu.edu>
2024-03-25 15:25:26 -0400

    Added Password Recovery Component

---
+++ src/PasswordRecovery.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import {getAuth, sendPasswordResetEmail} from 'firebase/auth';
+import app from './firebase.js';
+const PasswordRecovery = () =>{
+
+    const [email, setEmail] = useState('');
+    const auth = getAuth();
+
+    const sendReset = async() => {
+        await sendPasswordResetEmail(auth, email);
+    }
+   
+    return (
+    <>
+    <div className = "resetPage">
+        <input type="text" placeholder = "Email:" value = {email} 
+        onChange = {(e)=>setEmail(e.target.value)} />
+        <button className = "button" type = "button" onClick = {sendReset}>
+        Reset Password
+        </button>
+
+    </div>
+    </>
+   );
+}
+export default PasswordRecovery;
