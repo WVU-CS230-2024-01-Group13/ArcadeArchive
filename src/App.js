89799552eddf2bdb9228d2383384edae3b457c5d
nrm00014 <143005799+nrm00014@users.noreply.github.com>
2024-04-14 13:25:14 -0400

    Friends Component Partially complete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Analytics from './pages/Analytics';
 import PasswordRecovery from './pages/PasswordRecovery';
 import profView from './pages/profView';
 import profSettingsPage from './pages/profSettings';
+import ExploreView from './pages/ExploreView';
+import Login from './pages/Login';
 import logo from './logo.svg';
+import Friends from './pages/Friends';
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 import {Routes, Route} from 'react-router-dom';
@@ -13,11 +16,13 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path = "/" element={<CreatorView/>}/>
+            <Route path = "/" element={<Login/>}/> 
+            <Route path = "/create" element={<CreatorView/>}/>
             <Route path = "/analytics" element={<Analytics/>}/>
             <Route path = "/profile" element={<profView/>}/>
             <Route path = "/reset" element = {<PasswordRecovery/>}/>
             <Route path = "/explore" element = {<ExploreView/>}/>
+            <Route path = "/friends" element = {<Friends/>}/>
           </Routes>
         </BrowserRouter>
       </div>
