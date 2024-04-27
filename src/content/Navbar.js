603c17b374b8137a4132a93f2e8651242092ef42
ShaneRRichards <srr00020@mix.wvu.edu>
2024-04-27 18:41:14 -0400

    Update Navbar.js

--- src/content/Navbar.js
+++ src/content/Navbar.js
@@ -8,10 +8,9 @@ function Navbar() {
   // Check if the current route is the welcome view
   const isLoginView = location.pathname === '/login';
   const isSignupView = location.pathname === '/signup';
-  const isLandingView = location.pathname ==='/';
-
+  
   // If it's the welcome view, don't render the navbar
-  if (isLoginView || isSignupView || isLandingView) {
+  if (isLoginView || isSignupView) {
     return null;
   }
 
@@ -23,13 +22,12 @@ function Navbar() {
     </Link>
 
     <div class="navbar-menu">
-      <Link to="/explore" activeClassName="active">Explore</Link>
+      <Link to="/" activeClassName="active">Explore</Link>
       <Link to="/create" activeClassName="active">Create</Link>
       <Link to="/analytics" activeClassName="active">Analytics</Link>
       <Link to="/social" activeClassName="active">Social</Link>
       <Link to="/profile" activeClassName="active">Profile</Link>
       <Link to="/friends" activeClassName = "active">Friends</Link>
-      <Link to="/" activeClassName="active">Logout</Link>
     </div>
     <div class="greeting">Hello <span id="username">User</span>.</div>
   </div>
