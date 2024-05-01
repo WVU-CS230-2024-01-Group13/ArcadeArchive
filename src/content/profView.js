2c36875536b7ee0b796227023c6f38018cb25b1e
connorblevins <cb52@lockrmail.com>
2024-05-01 02:31:10 -0400

    updated profView and profSettings
    finished profView and profSettings, updated navBar too

--- src/content/profView.js
+++ src/content/profView.js
@@ -1,14 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useAuth } from '../contexts/AuthContext';
-import { dbSignup } from '../contexts/dbContext'
-import { Alert } from "react-bootstrap"
 import { Link, useNavigate } from 'react-router-dom';
+import { db, storage } from '../firebase';
+import { ref, get } from 'firebase/database';
+import { ref as sRef, getDownloadURL } from 'firebase/storage';
+import './styles.css'
+import './profSettingsPage.css'
+import { Form } from 'react-bootstrap';
 
-export default function ProfView () {
+
+export default function ProfView() {
 
     const { currentUser, logout } = useAuth()
+    const [userData, setUserData] = useState(null)
+    const [photoURL, setPhotoURL] = useState('')
     const [error, setError] = useState("")
     const navigate = useNavigate()
+
+    useEffect(() => {
+        if (currentUser) {
+            const userRef = ref(db, `users/${currentUser.uid}`);
+            get(userRef)
+                .then(snapshot => {
+                    if (snapshot.exists()) {
+                        setUserData(snapshot.val());
+                    } else {
+                        console.log('No such document!');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error getting document:', error);
+                });
+            getDownloadURL(sRef(storage, `profilePictures/${currentUser.uid}`)).then(snapshot => {
+                setPhotoURL(snapshot)
+            });
+        }
+    }, [currentUser]);
+
 
     async function handleLogout() {
         setError("")
@@ -20,19 +48,46 @@ export default function ProfView () {
             setError("Failed to log out")
         }
     }
-    
-    return (
-        <>
-            <h2  className='text-center mb-4'>Profile</h2>
-            {error && <Alert variant='danger'>{error}</Alert>}
-            <img src={currentUser.profPic} alt="Profile" />
-            <h1 classname='text-center mb-4'>{currentUser.username ? currentUser.username : currentUser.email}'s Profile</h1>
-            <p>{currentUser.bio ? currentUser.bio : "Update Bio to Display Here"}</p>
-            <Link to='/profile/settings'><button class="btn btn-secondary" >Edit Profile</button ></Link>
 
-            <div className="w-100 text-center mt-2">
-                <button class='btn btn-secondary' variant="link" onClick={handleLogout}>Log Out</button>
-            </div>
-        </>
+    return (
+        <div>
+            {error && <div>{error}</div>}
+            {userData && (
+
+
+                <div>
+                    <Form style={{ marginBottom: 500 }}>
+                        <div style={{ padding: 10 }}>
+                            <Form.Group>
+                                <div className="profileView-picture">
+                                    <img src={photoURL} alt="Profile" />
+                                </div>
+                            </Form.Group>
+                        </div>
+                        <div style={{ padding: 10 }}>
+                            <Form.Group className="mb-2">
+                                <h1 className="jersey-15-regular">{userData.username ? userData.username : userData.email}'s Profile</h1>
+                            </Form.Group>
+                        </div>
+                        <div style={{ padding: 10 }}>
+                            <Form.Group className="mb-2">
+                                <p className="jersey-15-regular" style={{ fontSize: 20 }}>{userData.bio ? userData.bio : "Update Bio to Display Here"}</p>
+                                <div style={{ padding: 10 }}>
+                                    <Link to={"/profile/settings"}><button className="jersey-15-regular" style={{ fontSize: 30 }}>Edit Profile</button ></Link>
+                                </div>
+                                <div style={{ padding: 10 }}>
+                                    <button className="jersey-15-regular" style={{ fontSize: 30 }} onClick={handleLogout}>Log Out</button >
+                                </div>
+                            </Form.Group>
+                        </div>
+                    </Form>
+
+                </div>
+            )}
+        </div>
+
     );
 }
+
+
+
